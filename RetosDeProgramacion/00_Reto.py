cbe0b8da4b5ba433db4512f513e88a3a9b444e8f
Carlos Alcala <c.alcala@outlook.com>
2024-04-07 17:17:02 +0200

    Adding exercise 0

--- RetosDeProgramacion/00_Reto.py
+++ RetosDeProgramacion/00_Reto.py
@@ -13,6 +13,31 @@
  * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
  """
 
+#0.1  Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
 #https://www.python.org/
 
 
+#0.2 - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+#Comentario de una linea
+"""
+Comentario
+Multilinea
+"""
+
+#0.3 Crea una variable (y una constante si el lenguaje lo soporta).
+#    Crea variables representando todos los tipos de datos primitivos
+first_name = "Carlos"
+last_name = "Alcala"
+number_of_sneakers = 20
+is_Active = True
+fte = 0.8
+array_example = ["first", "second", "third"]
+language_name = "Python"
+complex_num = 1+3j
+
+
+#0.4 Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+print("¡Hola,",language_name,"!")
